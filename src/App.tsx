4383cf8a6c371f654ea420b977b7eddbc8922b5a
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-01-29 19:27:36 -0600

    :tada:build(index):add first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1>Work in progress...</h1>
+      <h2>This is a temporal placeholder</h2>
+      <h2>This portfolio will be ready at the first week of February, 2024</h2>
+      <h3>Thanks for your patience</h3>
+    </>
+  )
+}
+
+export default App
