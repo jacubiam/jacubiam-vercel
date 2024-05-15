60ca80757da896c7e18df9c8e5c6ee74b40defef
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-05-15 14:35:33 -0600

    :recycle:refactor(about):edit about me text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,7 +3,7 @@ const AboutMe = () => {
     <section id="about-me" className="text-normal max-w-[895px] mx-3 pt-16 md:mx-auto lg:pt-20">
       <h1 className="font-title font-bold text-heading text-[calc(8px+5vw)] drop-shadow-title shadow-title mb-4 ml-2 min-[425px]:text-3xl md:text-[32px] md:ml-8 lg:ml-14">About me</h1>
       <div className="text-[calc(5vw)] mx-1 min-[425px]:text-xl min-[425px]:text-pretty sm:mx-6 md:mx-16 lg:ml-20">
-        <p>I'm a 27-year-old man from El Salvador, currently studying at Kodigo but ready to get experience in real world scenarios,
+        <p>I'm a web dev from El Salvador, currently studying at Kodigo but ready to get experience in real world scenarios,
           my commitment with the work it's clear and I know I'm ready to collaborate in important projects with amazing teams.</p>
         <br />
         <p>My skills range from abstract
